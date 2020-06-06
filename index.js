f0307cf1d39647e9c4eb536c71f65a60dcdc0598
aam051102 <ahlgreenmadsen@gmail.com>
2020-06-06 19:13:51 +0200

    Update gitignore. Fix index.js.

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ const { default: createZXP } = require("create-zxp");
 const path = require("path");
 
 createZXP({
-    inputDirectory: path.join(__dirname, "extension"),
-    outputDirectory: path.join(__dirname, "build"),
-    tempDir: path.join(__dirname, "temp"),
+    inputDirectory: "./extension",
+    outputDirectory: "./build",
+    tempDir: "./temp",
     version: 1.0,
     extractDirectory: path.join(__dirname, "debug/zxp"),
 }).catch((e) => console.error(e));
-
-/*
-buildoptions { "/Zc:wchar_t-" }
-postbuildcommands { "ren $(SolutionDir)extension\\plugin\\lib\\win\\$(TargetName).dll $(SolutionDir)extension\\plugin\\lib\\win\\$(TargetName).fcm" }
-*/
